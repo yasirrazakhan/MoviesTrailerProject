f5f4164487d792d1ad686bfe82e2b75f0bdfa7dc
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-04-26 20:09:40 +0500

    add media to track

---
+++ media.py
@@ -0,0 +1,10 @@
+
+#A template class for all movie objects
+class Movies():
+    def __init__(self,movie_title, movie_storyline, trailer_youtube, movie_poster):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.trailer_youtube_url = trailer_youtube
+        self.poster_image = movie_poster
+        
+    
