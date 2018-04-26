4437acbd8538868d55e73a6fa2436a3d55ee412e
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-04-26 22:59:52 +0500

    ensure code is inline with python style guide

--- media.py
+++ media.py
@@ -1,10 +1,12 @@
-
-#A template class for all movie objects
+# A template to create movie movie objects
 class Movies():
-    def __init__(self,movie_title, movie_storyline, trailer_youtube, movie_poster):
+    def __init__(
+                 self, movie_title,
+                 movie_storyline,
+                 trailer_youtube,
+                 movie_poster
+                 ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.trailer_youtube_url = trailer_youtube
         self.poster_image = movie_poster
-        
-    
