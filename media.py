534b91c5ff032e58aa3a045d0ca9c12a264bd388
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-04-27 12:18:34 +0500

    add comment about each attribute of the Movies class

--- media.py
+++ media.py
@@ -1,12 +1,17 @@
 # A template to create multiple movie objects
 class Movies():
     def __init__(
-                 self, movie_title,
+                 self,
+                 movie_title,
                  movie_storyline,
                  trailer_youtube,
                  movie_poster
                  ):
         self.title = movie_title
+        # Represent the title of the movie object
         self.storyline = movie_storyline
+        # Represent the storyline of the movie object
         self.trailer_youtube_url = trailer_youtube
+        # Represent the youtube url of the movie object
         self.poster_image = movie_poster
+        # Represent the poster image of the movie object
