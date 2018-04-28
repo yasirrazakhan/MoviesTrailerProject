e1fec75cdc9fcbf8390b523937bdcc6e93e2b53a
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-04-28 15:27:29 +0500

    add comment's and docstrings for variable

--- media.py
+++ media.py
@@ -7,6 +7,15 @@ class Movies():
                  trailer_youtube,
                  movie_poster
                  ):
+        """Details of Arguments supplied to __init__ method.
+
+        Args:
+            movie_title(str): Title of the movie.
+            movie_storyline(str): Story Line of the movie.
+            trailer_youtube(str): Youtube Trailer Url of the movie.
+            movie_poster(str): Poster image Url of the movie.
+
+        """
         self.title = movie_title  # The title of the movie object
         self.storyline = movie_storyline  # The storyline of the movie object
         self.trailer_youtube_url = trailer_youtube  # The trailer url of movie
